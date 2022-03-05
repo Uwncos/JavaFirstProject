526a90f6f1dad13e6f118d945a318acba1d71786
sasha <sasha06122002@gmail.com>
2022-03-05 19:36:38 +0300

    Add delete

--- src/main/java/binarySearchTree/Node.java
+++ src/main/java/binarySearchTree/Node.java
@@ -1,27 +1,17 @@
 package binarySearchTree;
 
 public class Node {
-    public int keyData;
+    public int key;
     public int data;
     public Node rightChild;
     public Node leftChild;
 
+    Node(int key, int data) {
+        this.key = key;
+        this.data = data;
+    }
+
     public void nodePrint() {
-        System.out.println("Node value:" + keyData);
-    }
-    public int getKey() {
-        return this.keyData;
-    }
-    public Node getLeftChild() {
-        return this.leftChild;
-    }
-    public Node getRightChild() {             //геттер
-        return this.rightChild;
-    }
-    public void setLeftChild() {
-        this.leftChild = leftChild;
-    }
-    public void setRightChild() {             //сеттер
-        this.rightChild = rightChild;
+        System.out.println("Node value:" + key);
     }
 }
--- src/main/java/binarySearchTree/binaryTree.java
+++ src/main/java/binarySearchTree/binaryTree.java
@@ -12,18 +12,19 @@ package binarySearchTree;
 public class binaryTree {
     public static void main(String[] args) {
     }
-    private Node root;
+
+    Node root;
 
     public binaryTree() { //Constructor for BST
         root = null;
     }
+
     public Node find(int key) {
         Node current = root;
-        while (current.keyData != key) {
-            if (key < current.keyData) {
+        while (current.key != key) {
+            if (key < current.key) {
                 current = current.leftChild;
-            }
-            else {
+            } else {
                 current = current.rightChild;
             }
             if (current == null) {
@@ -33,22 +34,24 @@ public class binaryTree {
         return current;
     }
 
-    public void insert(int insertKey, int inserData) {
-        Node nextNode = new Node();
-        nextNode.keyData = insertKey;
-        nextNode.data = inserData;
+    public void insert(int key, int data) {
+        Node nextNode = new Node(key, data);
+        //nextNode.key = key;
+        //nextNode.data = data;
         if (root == null) {
             root = nextNode;
-        }
-        else {
+        } else {
             Node current = root;
+            Node parent;
             while (true) {
-                Node parent = current;
-                if (insertKey < current.keyData) {
-                    parent.leftChild = nextNode;
-                    return;
-                }
-                else {
+                parent = current;
+                if (key < current.key) {
+                    current = current.leftChild;
+                    if (current == null) {
+                        parent.rightChild = nextNode;
+                        return;
+                    }
+                } else {
                     current = current.rightChild;
                     if (current == null) {
                         parent.rightChild = nextNode;
@@ -58,4 +61,101 @@ public class binaryTree {
             }
         }
     }
+
+    public boolean delete(int key) {
+        Node current = root;
+        Node parent = root;
+
+        boolean isLeft = true;
+        while (current.key != key) {
+            parent = current;
+
+            if (key < current.key) {
+                isLeft = true;
+                current = current.leftChild;
+            } else {
+                isLeft = false;
+                current = current.rightChild;
+            }
+            if (current == null) {
+                return false;
+            }
+        }
+        if (current.rightChild == null && current.leftChild == null) { //Node Is A Leaf Node
+            if (current == root) {
+                root = null;
+            } else if (isLeft == true) {
+                parent.leftChild = null;
+            } else {
+                parent.rightChild = null;
+            }
+        } else if (current.rightChild == null) {
+            if (current == root) {
+                root = current.leftChild;
+            } else if (isLeft == true) {
+                parent.leftChild = current.leftChild;
+            } else {
+                parent.rightChild = current.leftChild;
+            }
+        } else if (current.leftChild == null) {
+            if (current == root) {
+                root = current.rightChild;
+            } else if (isLeft == true) {
+                parent.leftChild = current.rightChild;
+            } else {
+                parent.rightChild = current.leftChild;
+            }
+        }
+        else {
+            Node toMove = getMove(current);
+            if (current == root) {
+                root = toMove;
+            }
+            else if (isLeft == true) {
+                parent.leftChild = toMove;
+            }
+            else {
+                parent.rightChild = toMove;
+            }
+            toMove.leftChild = current.leftChild;
+        }
+        return true;
+    }
+
+    public Node getMove(Node movedNode) {
+        Node moveParent = movedNode;
+        Node toMove = movedNode;
+        Node current = movedNode.rightChild;
+
+        while (current != null){
+            moveParent = toMove;
+            toMove = current;
+            current = current.leftChild;
+        }
+        if (toMove != movedNode.rightChild) {
+            moveParent.leftChild = toMove.rightChild;
+            toMove.rightChild = movedNode.rightChild;
+        }
+        return toMove;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
